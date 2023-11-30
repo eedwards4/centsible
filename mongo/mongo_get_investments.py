30664ac45b9e2a4361c6e3f874175efab3cdb426
Ethan Edwards <evanevansonbusiness@gmail.com>
2023-11-30 00:36:13 -0800

    I REALLY like pymongo first thing that's worked in a hot minute

--- mongo/mongo_get_investments.py
+++ mongo/mongo_get_investments.py
@@ -11,10 +11,25 @@ def main():
     except OSError:
         pass
 
-    with open('db_output/investments.txt', 'w') as file:
-        file.write('Investments!\n')  # TEMP
-
     # End file crap
+
+    # Connect to the database
+    client = pymongo.MongoClient('mongodb+srv://cent:ArZrGZ9m3lQ4tYlW@centsible0.i6gri7x.mongodb.net/?retryWrites=true&w=majority')
+
+    db = client.data
+
+    collection = db['investments']
+
+    test = collection.find_one({"ticker": "MSFT"})
+
+    with open('db_output/investments.txt', 'w') as file:
+        file.write(test["ticker"] + '\n')
+        file.write(str(test["num_shares"]) + '\n')
+        file.write(str(test["last_known_value"]) + '\n')
+        file.write(str(test["last_total_value"]) + '\n')
+        file.write(str(test["52_week_h"]) + '\n')
+        file.write(str(test["52_week_l"]) + '\n')
+        file.close()
 
 
 if __name__ == '__main__':
